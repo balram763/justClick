2ab4808f28e9be219f9ebd79492c2e5ed8efb7da
balram763 <balramdhakad763@gmail.com>
2024-08-08 02:45:26 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const Api_Key = 'e89dRvzVTtuodqkit9qeK4MC'
+let urlDownload;
+
+const handleSubmit  = async(Api_Key) => {
+    const inputFile = document.getElementById('pic') 
+    const Picture = inputFile.files[0]
+    // console.log(Picture)  
+    console.log(`clicked`)
+    
+    const formData = new FormData()
+    formData.append('image_file',Picture)
+    formData.append('size','auto')
+
+    const responce = await fetch('https://api.remove.bg/v1.0/removebg',{
+        method : 'POST',
+        headers : { "X-Api-Key": 'e89dRvzVTtuodqkit9qeK4MC' },
+        body : formData
+    })
+    // const blob = responce.blob()
+    .then(function(response){
+        return response.blob()
+    })
+    .then(function(blob){
+        const url = URL.createObjectURL(blob)
+        console.log(url)
+        const imageoutput = document.getElementById('imageoutput')
+        imageoutput.src = url;
+        urlDownload = url
+    }).catch()
+
+    const ui = document.getElementById('ui')
+    ui.className = 'card p-2 shadow'
+
+}
+
+
+const downloadimage = function(){
+
+    const a = document.createElement('a')
+    a.href = urlDownload
+    console.log(urlDownload)
+    a.download = 'download.png'
+    document.body.append(a)
+    a.click()
+    document.body.removeChild(a)
+}
